793f2aabdb2440f7e09ac800fb8c3e3d0218041f
Vanderlei Neto <josevanderleineto@outlook.com>
2025-08-13 09:07:49 -0300

    Ajustee

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,10 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: true // deixa o Neon cuidar do SSL
 });
 
 export default pool;
-
